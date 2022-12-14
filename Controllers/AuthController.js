7a83345cce4a746458c5592dd9bb6330f098307b
shuhaib <shshuaib99@gmail.com>
2022-12-14 10:59:59 +0530

    first commit

---
+++ Controllers/AuthController.js
@@ -0,0 +1,59 @@
+import UserModel from "../Models/userModel.js";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+
+//Registering a new User
+export const registerUser = async (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10)
+    const hashePass = await bcrypt.hash(password, salt)
+
+    const newUser = new UserModel({
+        firstname,
+        lastname,
+        email,
+        password: hashePass
+    })
+
+    try {
+        const user = await newUser.save()
+
+        const token = jwt.sign({
+            username: user.username, id: user._id
+        }, process.env.JWT_KEY, { expiresIn: '1h' })
+
+
+        res.status(200).json(user, token)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+}
+
+//Login User
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await UserModel.findOne({ email: email })
+        if (user) {
+            const validity = await bcrypt.compare(password, user.password)
+
+            if (!validity) {
+                res.status(400).json("Wrong Password")
+            } else {
+                const token = jwt.sign({
+                    username: user.username, id: user._id
+                }, process.env.JWT_KEY, { expiresIn: '1h' })
+                res.status(200).json(user,token)
+            }
+        }
+        else {
+            res.status(404).json("User Not Found")
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message }, "error on req.body in loginUser AuthContreller")
+    }
+}
---
+++ Models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+    { 
+        firstname: {
+            type: String,
+            required: true
+        },
+        lastname: {
+            type: String,
+
+        },
+        email: {
+            type: String,
+            required: true
+        },
+       
+        password: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        profilePicture: String,
+        coverPicture: String,
+        about: String,
+        livesIn: String,
+        worksAt: String,
+        relationship: String,
+        followers: [],
+        following: []
+    },
+    { timestamps: true }
+)
+
+const UserModel = mongoose.model('users', UserSchema)
+
+export default UserModel;
