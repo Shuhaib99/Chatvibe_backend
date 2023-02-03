89b306af9dd59a52a8c1d90f2a2430d44876797e
shuhaib <shshuaib99@gmail.com>
2023-02-03 18:09:40 +0530

    chat upds

--- Controllers/ChatController.js
+++ Controllers/ChatController.js
@@ -1,15 +1,22 @@
 import ChatModel from '../Models/ChatModel.js'
 
 export const createChat = async (req, res, next) => {
-    const senderid=req.userid
+    console.log(req.body);
+    const senderid = req.userid
     const newChat = new ChatModel({
         members: [senderid, req.body.recieverid]
     })
 
     try {
-        const result = await newChat.save()
-        res.status(200).json(result)
-
+        const chat = await ChatModel.findOne({
+            members: { $all: [req.userid, req.body.recieverid] }
+        })
+        if (!chat) {
+            const result = await newChat.save()
+            res.status(200).json({ result: result })
+        }else{
+            res.status(200).json({ result: chat })
+        }
     } catch (error) {
         res.status(500).json(error)
     }
@@ -20,7 +27,7 @@ export const userChats = async (req, res, next) => {
         const chat = await ChatModel.find({
             members: { $in: [req.userid] }
         })
-        res.status(200).json({chat,user:req.userid})
+        res.status(200).json({ chat, user: req.userid })
     } catch (error) {
         res.status(500).json(error)
     }
