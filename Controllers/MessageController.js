11a7532643c42dec38665d4d974492bb8d048bd5
shuhaib <shshuaib99@gmail.com>
2023-02-07 19:19:13 +0530

    mssg upds

--- Controllers/MessageController.js
+++ Controllers/MessageController.js
@@ -1,31 +1,32 @@
 import MessageModel from "../Models/MessageModel.js";
 
-export const addMessage = async(req,res,next) => {
-    const senderid =req.userid
-    const {chatid,text } = req.body
+export const addMessage = async (req, res, next) => {
+
+    const senderid = req.userid
+    const { chatid, text } = req.body
     const message = new MessageModel({
         chatid,
         senderid,
         text
 
     })
-    try{
-        const result =await message.save()
+    try {
+        const result = await message.save()
         res.status(200).json(result)
-    }catch (error) {
+    } catch (error) {
         res.status(500).json(error)
     }
 }
 
 
-export const getMessages = async(req,res,next)=>{
-   
-    try
-    {
-     const result = await MessageModel.find(req.params)   
-     res.status(200).json(result)
-    }catch (error) {
+export const getMessages = async (req, res, next) => {
+
+    try {
+        const chatid = req.params.chatId
+        const result = await MessageModel.find({ chatid: chatid })
+        res.status(200).json(result)
+    } catch (error) {
         res.status(500).json(error)
-       
+
     }
 }
